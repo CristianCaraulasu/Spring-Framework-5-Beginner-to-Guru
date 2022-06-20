a89efde2fb219fe82dcaa0a6f1c01c5128285b80
CristianCaraulasu <cristi_reyz@yahoo.com>
2022-06-20 13:56:14 +0300

    Added Profiles for map

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/services/map/OwnerMapService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/services/map/OwnerMapService.java
@@ -5,12 +5,14 @@ import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.services.OwnerService;
 import guru.springframework.sfgpetclinic.model.services.PetService;
 import guru.springframework.sfgpetclinic.model.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class OwnerMapService extends AbstractMapService<Owner,Long> implements OwnerService {
 
     private final PetTypeService petTypeService;
